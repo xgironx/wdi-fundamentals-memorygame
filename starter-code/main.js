d3f308c8b7b906561b70e961fb7a105c044e7436
xgironx <xgironx@gmail.com>
2017-01-12 14:19:46 -0500

    creaetated game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,9 @@
 console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+cardOne == cardFour
+cardOne == cardThree
+console.log("doneonedone!")
